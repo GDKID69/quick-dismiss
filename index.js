d722efe6bf7ad26de01b7761da990a7c05a99449
GDKID <bawbeefarted@gmail.com>
2022-08-26 15:39:19 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Plugin } = require("powercord/entities");
+const { getModule } = require("powercord/webpack");
+const { inject, uninject } = require("powercord/injector");
+const EphemeralAccessories = getModule(m => m.default?.displayName === "EphemeralAccessories", false);
+
+module.exports = class QuickDismiss extends Plugin {
+    startPlugin() {
+        inject("thisInject", EphemeralAccessories, "default", (args, res) => {
+            const msgNode = document.getElementById(`chat-messages-${args[0].message.id}`);
+            if (!msgNode) return res;
+
+            msgNode.addEventListener(
+                "contextmenu",
+                _ => res.props.children[1][1].props.onClick()
+            );
+
+            return res;
+        });
+    };
+
+    pluginWillUnload() {
+        uninject("thisInject");
+    };
+};
